15d8334dc771d56ac13a8a54cd8fecf9a4b9eda2
Vishal Paudel <95016059+vishalpaudel@users.noreply.github.com>
2023-06-21 17:18:02 +0530

    Modular nvim setup

---
+++ other/.config/nvim/lua/core/plugins.lua
@@ -0,0 +1,42 @@
+-- Bootstraping Packer
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  -- GUI enhancements
+  use 'nvim-tree/nvim-tree.lua'
+  use 'nvim-tree/nvim-web-devicons'
+  use 'nvim-lualine/lualine.nvim'
+
+  -- Finding
+
+  -- Colorscheme
+  use 'ellisonleao/gruvbox.nvim'
+
+  -- Language support
+  use {
+      'nvim-telescope/telescope',
+
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
